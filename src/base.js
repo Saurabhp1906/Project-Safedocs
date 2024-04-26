891d9ca65ca876104d7ebf6ac2680722e33ef2cf
Saurabh Pareek <105624462+Saurabhp1906@users.noreply.github.com>
2024-04-26 11:17:54 +0530

    Update base.js

--- src/base.js
+++ src/base.js
@@ -5,13 +5,13 @@ import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
   projectId: "safedocs-2023",
-  appId: "1:334248100368:web:07805650886a699cf1c095",
-  storageBucket: "safedocs-2023.appspot.com",
+  appId: "---",
+  storageBucket: "---",
   locationId: "us-central",
-  apiKey: "AIzaSyA16HuuQhpliAvlrhVLEr_y7n7QOtXMcj8",
-  authDomain: "safedocs-2023.firebaseapp.com",
-  messagingSenderId: "334248100368",
-  measurementId: "G-NX4QQ5FBQ3",
+  apiKey: "---",
+  authDomain: "---",
+  messagingSenderId: "---",
+  measurementId: "---",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
